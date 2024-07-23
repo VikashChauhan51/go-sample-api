7de37d5fc6dcebdb414764d753c9fc75f329d913
Vikash Chauhan <vikashchauhan51@gmail.com>
2024-07-23 21:02:14 +0530

    added db connection

--- internal/infra/databases/db.go
+++ internal/infra/databases/db.go
@@ -1,1 +1,14 @@
 package databases
+
+import (
+	"gorm.io/driver/sqlserver"
+	"gorm.io/gorm"
+)
+
+func NewDBConnection(dsn string) (*gorm.DB, error) {
+	db, err := gorm.Open(sqlserver.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
